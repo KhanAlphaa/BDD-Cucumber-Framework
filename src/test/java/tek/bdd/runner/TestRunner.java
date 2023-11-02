2599a91ec67cfe89b1e8264f4ca3071b52a8c284
KhanAlphaa <p.demo1990@gmail.com>
2023-11-02 16:34:17 -0600

    Ran another regression using TestRunner through Maven and pushed it to the Git repository.

--- src/test/java/tek/bdd/runner/TestRunner.java
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "classpath:features",
         glue = "tek.bdd.steps",
-        tags = "@Smoke_HomePageValidation",
+        tags = "@Regression_Login",
         dryRun = false, //if true wont run steps just check for un-implemented steps
         plugin = {
                 "html:target/html_report/report.html",
